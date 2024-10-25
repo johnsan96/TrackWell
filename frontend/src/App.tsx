7e760eab923202a31a32bf9930fe7fb183150675
johnsan96 <johnpauldoctolero@gmail.com>
2024-10-25 12:17:29 +0200

    add Dockerfile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Finance from './pages/Finance';
- 
+
 const App = () => {
    return (
       <>
          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/finance" element={<Finance />} />
-          
+            <Route path="/" element={<Finance />} />
+            <Route path="/tracking" element={<Home />} />
+
          </Routes>
       </>
    );
 };
- 
+
 export default App;
