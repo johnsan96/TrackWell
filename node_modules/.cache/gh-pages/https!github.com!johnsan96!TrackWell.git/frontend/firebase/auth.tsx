f484dcded0c9e2d67358491a79df22d5ce46f0a9
johnsan96 <johnpauldoctolero@gmail.com>
2024-11-29 23:09:11 +0100

    refactor

---
+++ node_modules/.cache/gh-pages/https!github.com!johnsan96!TrackWell.git/frontend/firebase/auth.tsx
@@ -0,0 +1,46 @@
+/* import { auth } from "./firebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+  sendEmailVerification,
+  updatePassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+export const doCreateUserWithEmailAndPassword = async (email: string, password: string) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithEmailAndPassword = (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  const result = await signInWithPopup(auth, provider);
+  const user = result.user;
+
+  // add user to firestore
+};
+
+export const doSignOut = () => {
+  return auth.signOut();
+};
+
+export const doPasswordReset = (email: string) => {
+  return sendPasswordResetEmail(auth, email);
+};
+
+export const doPasswordChange = (password: string) => {
+    // @ts-ignore
+  return updatePassword(auth.currentUser, password);
+};
+
+export const doSendEmailVerification = () => {
+    // @ts-ignore
+  return sendEmailVerification(auth.currentUser, {
+    url: `${window.location.origin}/home`,
+  });
+}; */
---
+++ node_modules/.cache/gh-pages/https!github.com!johnsan96!TrackWell.git/frontend/src/react-charts.d.ts
@@ -0,0 +1,5 @@
+// In src/react-charts.d.ts
+declare module "react-charts" {
+    const content: any; // Oder passen Sie dies an, falls spezifischere Typen bekannt sind
+    export default content;
+}
