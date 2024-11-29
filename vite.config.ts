6775baa586a03ffd895e723e54555b777153496b
johnsan96 <johnpauldoctolero@gmail.com>
2024-11-29 23:37:41 +0100

    ....

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TrackWell/",
+  base: "/TrackWell",
   server: {
     host: "0.0.0.0", // Damit der Server auf allen Netzwerk-Schnittstellen hört
     port: 5173, // Der Standard-Port für Vite
